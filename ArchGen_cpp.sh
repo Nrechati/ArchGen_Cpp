695cfa70abd9779996bee3f4f0e9033f23d3ad88
Hyphi <nrechati@student.42.fr>
2019-10-18 16:23:45 +0200

    [ArchGen] Auto-get ARCH_PATH from dirname

--- ArchGen_cpp.sh
+++ ArchGen_cpp.sh
@@ -6,7 +6,7 @@
 #    By: nrechati <nrechati@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/18 15:43:12 by nrechati          #+#    #+#              #
-#    Updated: 2019/10/18 15:52:17 by nrechati         ###   ########.fr        #
+#    Updated: 2019/10/18 16:23:25 by nrechati         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,39 +33,42 @@ ROBOT="c[○┬●]כ  "
 GENTLEMAN="ಠ_ರೃ  "
 
 printf "${LCYAN}\n${NC}"
-printf "${LCYAN}    ╔═╗╔═╗╔═╗  ╦┌┐┌┬┌┬┐    \n${NC}"
-printf "${LCYAN}    ║  ╠═╝╠═╝  ║││││ │     \n${NC}"
-printf "${LCYAN}    ╚═╝╩  ╩────╩┘└┘┴ ┴     \n${NC}"
+printf "${LCYAN}    ╔═╗┬─┐┌─┐┬ ┬╔═╗┌─┐┌┐┌  ╔═╗╔═╗╔═╗     \n${NC}"
+printf "${LCYAN}    ╠═╣├┬┘│  ├─┤║ ╦├┤ │││  ║  ╠═╝╠═╝     \n${NC}"
+printf "${LCYAN}    ╩ ╩┴└─└─┘┴ ┴╚═╝└─┘┘└┘  ╚═╝╩  ╩       \n${NC}"
 printf "${LCYAN}\n${NC}"
+printf "${LGREEN}${ROBOT}[Start]:\t\t${YELLOW}Welcome to ArchGen for C++ setup\n${NC}"
 
-ARCH_PATH=~/Work/Tools/ArchGen_Cpp
+#*********************************#
+ARCH_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+#*********************************#
 
 mkdir ./includes ./includes/core ./includes/class ./srcs ./srcs/core ./srcs/class
-cat $(echo "${ARCH_PATH}/files/Makefile_1") >> Makefile
-cat $(echo "${ARCH_PATH}/files/main_1") >> ./srcs/core/main.cpp
+cat $(echo "${ARCH_PATH}/srcs/Makefile_1") >> Makefile
+cat $(echo "${ARCH_PATH}/srcs/main_1") >> ./srcs/core/main.cpp
 while [ $# -ne 0 ]
 do
 	upper=$(echo $1 | tr a-z A-Z)
 	if [ ${1:0:2} != "I_" ]
 	then
-		printf "${LCYAN}${ROBOT}[new Class]:\t\t${LGREEN}Creating ${YELLOW}$1 ${LGREEN}Classe files (.hpp and .cpp)\n${NC}"
+		printf "${LCYAN}${ROBOT}[new Class]:\t\t${LGREEN}Creating ${YELLOW}$1 ${LGREEN}Classe srcs (.hpp and .cpp)\n${NC}"
 		touch ./srcs/class/$1.cpp ./includes/class/$1.hpp
-		cat $(echo "${ARCH_PATH}/files/class.cpp") >> ./srcs/class/$1.cpp
+		cat $(echo "${ARCH_PATH}/srcs/class.cpp") >> ./srcs/class/$1.cpp
 		sed -i '' "s/Sample/$1/g" ./srcs/class/$1.cpp
 		if [ ${1:0:2} != "A_" ]
 		then
-			cat $(echo "${ARCH_PATH}/files/class.hpp") >> ./includes/class/$1.hpp
+			cat $(echo "${ARCH_PATH}/srcs/class.hpp") >> ./includes/class/$1.hpp
 			sed -i '' "s/Sample/$1/g" ./includes/class/$1.hpp
 			sed -i '' "s/SAMPLE_H/${upper}_H/g" ./includes/class/$1.hpp
 		else
-			cat $(echo "${ARCH_PATH}/files/abstract.hpp") >> ./includes/class/$1.hpp
+			cat $(echo "${ARCH_PATH}/srcs/abstract.hpp") >> ./includes/class/$1.hpp
 			sed -i '' "s/Sample/$1/g" ./includes/class/$1.hpp
 			sed -i '' "s/SAMPLE_H/${upper}_H/g" ./includes/class/$1.hpp
 		fi
 		echo "class_src += $1.cpp" >> Makefile
 	else
-		printf "${LPURPLE}${ROBOT}[new Interface]:\t${LGREEN}Creating ${YELLOW}$1 ${LGREEN}Interface files (.hpp and .cpp)\n${NC}"
-		cat $(echo "${ARCH_PATH}/files/interface.hpp") >> ./includes/class/$1.hpp
+		printf "${LPURPLE}${ROBOT}[new Interface]:\t${LGREEN}Creating ${YELLOW}$1 ${LGREEN}Interface srcs (.hpp and .cpp)\n${NC}"
+		cat $(echo "${ARCH_PATH}/srcs/interface.hpp") >> ./includes/class/$1.hpp
 		sed -i '' "s/Sample/$1/g" ./includes/class/$1.hpp
 		sed -i '' "s/SAMPLE_H/${upper}_H/g" ./includes/class/$1.hpp
 	fi
@@ -75,9 +78,9 @@ do
 done
 printf "\n"
 printf "${LRED}${ROBOT}[Makefile]:\t\tCreating project Makefile\n${NC}"
-cat $(echo "${ARCH_PATH}/files/Makefile_2") >> Makefile
+cat $(echo "${ARCH_PATH}/srcs/Makefile_2") >> Makefile
 printf "${LRED}${ROBOT}[Main.cpp]:\t\tCreating project placeholder main.cpp\n${NC}"
-cat $(echo "${ARCH_PATH}/files/main_2") >> ./srcs/core/main.cpp
+cat $(echo "${ARCH_PATH}/srcs/main_2") >> ./srcs/core/main.cpp
 printf "\n"
 printf "${LCYAN}${ROBOT}[Binary name]: How you like to call your Binary ? $>  ${NC}"
 read name
